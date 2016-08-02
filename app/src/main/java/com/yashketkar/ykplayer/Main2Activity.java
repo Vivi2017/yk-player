14916201fa468bff7a4d35b56ca01e749d619b33
Yash Ketkar <yashketkar@hotmail.com>
2016-08-02 13:29:14 -0600

    Update to Main2 Activity

--- app/src/main/java/com/yashketkar/ykplayer/Main2Activity.java
+++ app/src/main/java/com/yashketkar/ykplayer/Main2Activity.java
@@ -80,17 +80,17 @@ public class Main2Activity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_videos) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_network_stream) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_torrent_stream) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_live_tv) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_website) {
 
         }
 
